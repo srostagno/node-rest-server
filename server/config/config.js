0eda444aa51c15222190765daffe455f898859b3
“srostagno” <srostagno@gmail.comssh-keygen -t rsa -C srostagno@gmail.comcat>
2019-06-11 17:17:16 -0400

    change urlDB prod

--- server/config/config.js
+++ server/config/config.js
@@ -19,8 +19,7 @@ let urlDB;
 if (process.env.NODE_ENV === "dev") {
   urlDB = "mongodb://localhost:27017/cafe";
 } else {
-  urlDB =
-    "mongodb//strider: AGs3qPgL7Iu1fJUZ@cluster0-a0t0t.mongodb.net/tester";
+  urlDB = "mongodb//strider:AGs3qPgL7Iu1fJUZ@cluster0-a0t0t.mongodb.net/tester";
 }
 
 process.env.NODE_ENV = urlDB;
